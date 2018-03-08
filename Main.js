756bbe0d3ad8079f651a084c445dce2d4136df39
DamienMadaule <damienmadaule@free.fr>
2018-03-08 10:31:44 +0100

    initialisation

---
+++ Main.js
@@ -0,0 +1,12 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function create(){
+
+}
+function update(){
+
+}
+
+function render(){
+    
+}
